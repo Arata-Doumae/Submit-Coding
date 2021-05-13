97012f5576ca147a3222573fc94306acfb91d2ff
Arata-Doumae <shikaisoku225@icloud.com>
2021-05-13 10:26:31 +0900

    first submit

---
+++ js/my page.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const textChange = () => {
+    document.getElementById('target').textContent='こんにちは！筒前 新大（どうまえあらた）です。';
+}
+setTimeout(textChange,2000);
+
+
+
+const showElementAnimation = () => {
+
+    let element = document.getElementsByClassName('title');
+    if(!element) return;
+
+    let showTiming = window.innerHeight > 768 ? 200 : 40;
+    let scrollY = window.pageYOffset;
+    let windowH = window.innerHeight;
+
+    for(let i=0;i<element.length;i++) { 
+        let elemClientRect = element[i].getBoundingClientRect(); let elemY = scrollY + elemClientRect.top; 
+        if(scrollY + windowH - showTiming > elemY) {
+        element[i].classList.add('is-show');
+      } else if(scrollY + windowH < elemY) ;
+    }
+  }
+  showElementAnimation();
+  window.addEventListener('scroll', showElementAnimation);
+
